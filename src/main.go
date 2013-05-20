cb5c6a597c74d510254ad5593d99e00a2c2d7b0e
Dmitry Vovk <dmitry.vovk@gmail.com>
2013-05-20 15:27:01 +0300

    * Add watcher to subscribe/ignore tubes on the fly * Add timed out reservation to allow watcher to run when there are no jobs * Add lost connection recovery

--- src/main.go
+++ src/main.go
@@ -18,6 +18,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -27,25 +28,33 @@ var server = flag.String("connect", "0.0.0.0:11300", "Address:port of beanstalkd
 /** Directory containing executable workers */
 var workersPath = flag.String("workers", "./workers/", "Directory path with worker scripts. Default: ./workers/")
 
+var tubes []string
+
 /** Interval in milliseconds between queue check */
 var interval time.Duration = 100
 
 /** Delay after queue error */
 var errorDelay time.Duration = 1000
 
+/** Delay after failed attempt to (re)connect to beanstalkd */
+var reconnectDelay time.Duration = 5000
+
 /** Worker runs counter */
 var workerRuns uint64 = 1
 
-/** Number of running workers */
-var workersCount uint = 0
+/** Number of running workers for each tube */
+var workersCount map[string]uint
+
+/** Beanstalkd server connection */
+var beanstalkd *lentil.Beanstalkd
 
 /**
  * Process to run worker and collect output
  */
 func workerRunner(channel chan string, instance uint64) {
 	// Get worker name
-	worker := <-channel
-	workersCount++
+	worker := <- channel
+	workersCount[worker]++
 	log.Printf("Starting worker %s:%d\n", worker, instance)
 	var out bytes.Buffer
 	cmd := exec.Command(worker, "")
@@ -55,7 +64,84 @@ func workerRunner(channel chan string, instance uint64) {
 		log.Printf("Worker %s:%d returned an error: %s", worker, instance, error)
 	}
 	log.Printf("Worker %s:%d output: %s", worker, instance, out.String())
-	workersCount--
+	workersCount[worker]--
+}
+
+/**
+ * Try to connect to beanstalkd until successfully connected
+ */
+func connect() *lentil.Beanstalkd {
+	for {
+		log.Printf("Connecting to %s", *server)
+		beanstalkd, err := lentil.Dial(*server)
+		if err != nil {
+			log.Print(err)
+			time.Sleep(reconnectDelay * time.Millisecond)
+			continue
+		}
+		log.Printf("Connected to %s", *server)
+		return beanstalkd
+	}
+}
+
+/**
+ * Subscribe to queue tubes
+ */
+func subscribe(tubes []string) {
+	for i, _ := range tubes {
+		_, err := beanstalkd.Watch(tubes[i])
+		if err != nil {
+			log.Print(err)
+		} else {
+			log.Printf("Subscribed for %s\n", tubes[i])
+		}
+	}
+}
+
+/**
+ * Looks for workers in specified directory
+ */
+func listWorkers() []string {
+	tubes, err := filepath.Glob("*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tubes
+}
+
+/**
+ * Watches for changes in workers, and subscribes on the fly
+ */
+func watcher() {
+	workers, _ := filepath.Glob("*")
+	watchedTubes, _ := beanstalkd.ListTubesWatched()
+	mWatched := make(map[string]bool)
+	for _, watchedTube := range watchedTubes {
+		if watchedTube != "default" {
+			mWatched[watchedTube] = true
+		}
+	}
+	mWorkers := make(map[string]bool)
+	for _, worker := range workers {
+		mWorkers[worker] = true
+	}
+	for worker, _ := range mWorkers {
+		// Check if we need to subscribe to new tube
+		if _, has := mWatched[worker]; !has {
+			log.Printf("Subscribing to %s", worker)
+			beanstalkd.Watch(worker)
+			workersCount[worker] = 0
+		}
+	}
+	for tube, _ := range mWatched {
+		// Check if we no longer have worker
+		if _, has := mWorkers[tube]; !has {
+			log.Printf("Ignoring %s", tube)
+			beanstalkd.Ignore(tube)
+			delete(workersCount, tube)
+		}
+	}
+	tubes = workers
 }
 
 /**
@@ -66,56 +152,48 @@ func main() {
 	flag.Parse()
 
 	// Connect to beanstalkd
-	log.Printf("Connecting to %s", *server)
-	beanstalkd, error := lentil.Dial(*server)
-	if error != nil {
-		log.Fatal(error)
+	beanstalkd = connect()
+
+	// Create map for running worker counts
+	workersCount = make(map[string]uint)
+
+	// Go to workers dir
+	err := os.Chdir(*workersPath)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Get list of available worker scripts
-	log.Printf("Workers directory: %s", *workersPath)
-	error = os.Chdir(*workersPath)
-	if error != nil {
-		log.Fatal(error)
-	}
-	workerFiles, error := filepath.Glob("*")
-	if error != nil {
-		log.Fatal(error)
-	}
-
-	// Subscribe to tubes. TubeName == WorkerName
-	for worker := range workerFiles {
-		_, error := beanstalkd.Watch(workerFiles[worker])
-		if error != nil {
-			log.Fatal(error)
-		} else {
-			log.Printf("Subscribed for %s\n", workerFiles[worker])
-		}
-	}
+	// Subscribe to queues
+	watcher()
 
 	// Create communications channel
 	channel := make(chan string)
 
 	// Wait for jobs
-	for true {
-		job, error := beanstalkd.Reserve()
-		if error != nil {
-			log.Printf("Error reserving a job: %s", error)
-			time.Sleep(errorDelay * time.Millisecond)
-			continue
+	for {
+		job, err := beanstalkd.ReserveWithTimeout(1)
+		if err != nil {
+			var errType string = err.Error()
+			// Time out error is ok
+			if !strings.Contains(errType, "TIMED_OUT") {
+				log.Printf("Error reserving a job: %s", errType)
+				beanstalkd.Quit()
+				beanstalkd = connect()
+				subscribe(tubes)
+			}
 		} else {
 			// Got job, get tube name
-			stats, error := beanstalkd.StatsJob(job.Id)
-			if error != nil {
-				log.Printf("Error retrieving job %d stats: %s", job.Id, error)
+			stats, err := beanstalkd.StatsJob(job.Id)
+			if err != nil {
+				log.Printf("Error retrieving job %d stats: %s", job.Id, err)
 				time.Sleep(errorDelay * time.Millisecond)
 				continue
 			}
 			var worker string = stats["tube"]
 			// Put job back to queue
-			error = beanstalkd.Release(job.Id, 0, 0)
-			if error != nil {
-				log.Printf("Error releasing job %d: %s", job.Id, error)
+			err = beanstalkd.Release(job.Id, 0, 0)
+			if err != nil {
+				log.Printf("Error releasing job %d: %s", job.Id, err)
 				time.Sleep(errorDelay * time.Millisecond)
 				continue
 			}
@@ -124,7 +202,11 @@ func main() {
 			// Send worker the name of the tube to process
 			channel <- worker
 			// See how many workers we have running at the moment
-			log.Printf("Running workers count: %d\n", workersCount)
+			log.Printf("Running %s workers count: %d\n", worker, workersCount[worker])
+		}
+		if workerRuns % 5 == 0 {
+			// Run watcher each 5th tick
+			watcher()
 		}
 		// Be polite to system
 		time.Sleep(interval * time.Millisecond)
